e0707a647e64baec57c13967ad83b7faa9925615
Liu Lang <agi_liulang@163.com>
2020-05-18 21:45:30 +0800

    init projects

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPlugin({
+  template:path.join(__dirname,"src/index.html"),
+  filename:"index.html"
+})
+
+module.exports={
+  mode:'development',
+  plugins: [htmlPlugin],
+  entry:path.resolve(__dirname,'./src/index.js'),
+  module: {
+    rules: [
+      {test: /\.js|jsx$/,use:['babel-loader'], exclude: [path.resolve(__dirname,'node_modules')]},
+
+    ],
+  },
+  resolve:{
+    extensions:[".jsx",".js"],
+    alias: {
+      "@":path.join(__dirname,'./src')
+    }
+  }
+}
